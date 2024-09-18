32797a106f0863a961f417cf4eb22b0413d30b33
Cameron.Murphy <cameron.e.murphy@gmail.com>
2024-09-18 16:39:06 -0400

    Shapes2 start

---
+++ projects/Shapes2/Shapes2/src/edu/sinclair/cameron_murphy/Main.java
@@ -0,0 +1,11 @@
+package edu.sinclair.cameron_murphy;
+import javax.swing.JFrame;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		JFrame frame = new PictureFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}
---
+++ projects/Shapes2/Shapes2/src/edu/sinclair/cameron_murphy/Shape.java
@@ -0,0 +1,55 @@
+package edu.sinclair.cameron_murphy;
+
+import java.awt.*;
+
+public abstract class Shape {
+    private Color myColor;
+    private Point p1, p2;
+    private int lineWidth = 2;
+
+    public Shape() {
+        p1 = new Point(0, 0);
+        p2 = new Point(0, 0);
+        myColor = Color.BLACK;
+    }
+
+    public Shape(Point p1, Point p2, Color c) {
+        setP1(p1);
+        setP2(p2);
+        setColor(c);
+    }
+
+    public void setP1(Point p) {
+        p1 = p;
+    }
+
+    public void setP2(Point p) {
+        p2 = p;
+    }
+
+    public void setColor(Color c) {
+        this.myColor = c;
+    }
+
+    public Point getP1() {
+        return p1;
+    }
+
+    public Point getP2() {
+        return p2;
+    }
+
+    public Color getColor() {
+        return myColor;
+    }
+
+    public int getLineWidth() {
+        return lineWidth;
+    }
+
+    public void setLineWidth(int width) {
+        lineWidth = width;
+    }
+
+    public abstract void draw(Graphics g);
+}
