27079e17c5e4d42a0cf9d2b3604927b07ec078b5
CamJam91 <131709195+CamJam91@users.noreply.github.com>
2024-09-26 12:15:38 -0400

    Create Ant draft, Create preyAnimal interface

---
+++ projects/Classification/Classification/src/cameron_murphy/Ant.java
@@ -0,0 +1,60 @@
+package cameron_murphy;
+//Cameron Murphy CIS 2217 R01 9/25/2024
+//Ant class for creating ant objects
+public class Ant extends Species implements PreyAnimal, CasteAnimal {
+	private String name;
+	private Boolean eaten;
+	private enum Caste{
+		WORKER, SOLDIER, QUEEN
+	}
+	private Caste caste;
+	
+		//constructors
+	public Ant(String kingdom, String className, String order, String family, String genus, String species,
+			Boolean extinct, String name, String caste) {
+		super(kingdom, className, order, family, genus, species,extinct);
+		this.name = name;
+		setCaste(caste);
+	}
+	
+		//getters
+	public String getName() {
+		return name;
+	}
+
+			//interface overrides
+	@Override
+	public String getCaste() {
+		return caste.toString();
+	}
+	@Override
+	public Boolean getEaten() {
+		return eaten;
+	}
+
+		//setters
+	public void setName(String name) {
+		this.name = name;
+	}
+			//interface overrides
+	@Override
+	public void setCaste(String caste) {
+		this.caste = Caste.valueOf(caste.toUpperCase());
+	}
+	@Override
+	public void setEaten(Boolean eaten) {
+		this.eaten = eaten;
+	}
+	
+	@Override
+	public String toString() {
+		String preyStatus;
+		if (eaten)
+			preyStatus = "This animal has been eaten";
+		else
+			preyStatus = "This animal is alive";
+		return super.toString() + " Name: " + name + " Caste: " + String.valueOf(caste) + 
+	}
+
+	
+}
